8a9deb9a789b8a0f3431c65ad70b1b8048b69ae3
alagiehafigue <alagiehafigue@gmail.com>
2025-10-30 11:04:27 +0000

    Add meta title to our routes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,22 +1,35 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
+import { HeadContent, Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import { TanStackDevtools } from "@tanstack/react-devtools";
 
 export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        name: "description",
+        content:
+          "Share, explore and build on the best startup ideas and side hustles",
+      },
+      {
+        title: "IdeaDrop - Your Idea Hub",
+      },
+    ],
+  }),
   component: () => (
     <>
+      <HeadContent />
       <Outlet />
       <TanStackDevtools
         config={{
-          position: 'bottom-right',
+          position: "bottom-right",
         }}
         plugins={[
           {
-            name: 'Tanstack Router',
+            name: "Tanstack Router",
             render: <TanStackRouterDevtoolsPanel />,
           },
         ]}
       />
     </>
   ),
-})
+});
